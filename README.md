6c968a1e029ab70f91512a4dc72d996498731bf8
Nicolas Handke <nicolas.handke@gmx.de>
2024-08-18 12:33:00 +0200

    Finalize Inference-Server

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ docker build -f Dockerfile -t valueeval24-hierocles-of-alexandria:1.0.0 .
 
 # run
 tira-run \
-  --input-directory "$PWD/valueeval24/test"\
+  --input-directory "$PWD/valueeval24/test" \
   --output-directory "$PWD/output" \
   --image valueeval24-hierocles-of-alexandria:1.0.0
 
@@ -22,13 +22,41 @@ cat output/run.tsv
 ```
 
 ## Inference Server
+A local inference server can be started from the same Docker-Image using:
 ```bash
 PORT=8001
 
 docker run --rm -it --init \
-  -v "$PWD/logs:/workspace/logs" \
+  -v "$PWD/logs:/logs" \
   -p $PORT:$PORT \
-  --entrypoint tira-run-inference-server
+  --entrypoint tira-run-inference-server \
   valueeval24-hierocles-of-alexandria:1.0.0 \
   --script /predict.py --port $PORT
+
+# or, for zero-shot version
+docker run --rm -it --init \
+  -v "$PWD/logs:/logs" \
+  -p $PORT:$PORT \
+  -e HOA_ZERO_SHOT="True" \
+  --entrypoint tira-run-inference-server \
+  valueeval24-hierocles-of-alexandria:1.0.0 \
+  --script /predict.py --port $PORT
+docker run --rm -it --init -v "$PWD/logs:/logs" -p 8001:8001 -e HOA_ZERO_SHOT="True" --entrypoint tira-run-inference-server valueeval24-hierocles-of-alexandria:1.0.0 --script /predict.py --port 8001
 ```
+
+Exemplary request for a server running on localhost:8001 are
+
+```bash
+# POST (JSON list as payload)
+curl -X POST -H "application/json" \
+  -d "[{\"Text\": \"element 1\", \"language\": \"EN\"}, {\"Text\": \"element 2\", \"language\": \"EN\"}]" \
+  localhost:8001
+```
+and
+```bash
+# GET (JSON object string(s) passed to the 'payload' parameter)
+curl "localhost:8001?payload=\"element+1\"&payload=\"element+2\""
+```
+The possible values for language are `EN`, `EL`, `DE`, `TR`, `FR`, `BG`, `HE`, `IT`, `NL`.
+Please note that GET-request are currently only possible for language `EN`.
+
--- predict.py
+++ predict.py
@@ -1,7 +1,10 @@
+from typing import List
 import argparse
 import datasets
 import numpy
 import os
+
+import logging
 
 import numpy as np
 import pandas
@@ -22,7 +25,7 @@ warnings.filterwarnings("ignore")
 datasets.utils.logging.disable_progress_bar()
 
 
-ZERO_SHOT = bool(os.environ.get('HOA_ZERO_SHOT', False))
+ZERO_SHOT = os.environ.get('HOA_ZERO_SHOT', "False") == "True"
 
 
 ################################################################################
@@ -81,6 +84,11 @@ lang_dict = {'EN': 0,
              'NL': 8}
 id2lang_dict = {v: k for k, v in lang_dict.items()}
 
+test = {"Status": "OK", "output": [
+    {"Self-direction: thought attained": 0.0024574564304202795, "Self-direction: thought constrained": 0.00011691349209286273, "Self-direction: action attained": 0.0007427241653203964, "Self-direction: action constrained": 0.0009987016674131155, "Stimulation attained": 0.0016534713795408607, "Stimulation constrained": 7.240273407660425e-05, "Hedonism attained": 0.00011946013546548784, "Hedonism constrained": 5.626973870676011e-05, "Achievement attained": 0.0007442916976287961, "Achievement constrained": 0.0002864353300537914, "Power: dominance attained": 0.00047256675316020846, "Power: dominance constrained": 0.0005503477295860648, "Power: resources attained": 0.0026424031239002943, "Power: resources constrained": 0.0005643037147819996, "Face attained": 0.0009248544811271131, "Face constrained": 0.000765619392041117, "Security: personal attained": 0.0007875649025663733, "Security: personal constrained": 0.00041763478657230735, "Security: societal attained": 0.0015754187479615211, "Security: societal constrained": 0.0006250546430237591, "Tradition attained": 0.00037790153874084353, "Tradition constrained": 0.0001758920552674681, "Conformity: rules attained": 0.0008725103107281029, "Conformity: rules constrained": 0.0006714258925057948, "Conformity: interpersonal attained": 0.0003606232057791203, "Conformity: interpersonal constrained": 0.0008310032426379621, "Humility attained": 0.000631237868219614, "Humility constrained": 0.5000576972961426, "Benevolence: caring attained": 0.00016925728414207697, "Benevolence: caring constrained": 0.0002605092595331371, "Benevolence: dependability attained": 0.0004898284678347409, "Benevolence: dependability constrained": 0.00010994300828315318, "Universalism: concern attained": 0.0008231507381424308, "Universalism: concern constrained": 0.00043963169446215034, "Universalism: nature attained": 0.012091834098100662, "Universalism: nature constrained": 0.0008385402616113424, "Universalism: tolerance attained": 0.0003907356585841626, "Universalism: tolerance constrained": 0.0006092724506743252},
+    {"Self-direction: thought attained": 0.09426147490739822, "Self-direction: thought constrained": 0.0005233777337707579, "Self-direction: action attained": 0.003918548114597797, "Self-direction: action constrained": 0.00040401381556876004, "Stimulation attained": 0.026682263240218163, "Stimulation constrained": 0.0050332010723650455, "Hedonism attained": 0.0007003754726611078, "Hedonism constrained": 0.00013359410513658077, "Achievement attained": 0.01484023965895176, "Achievement constrained": 0.0032732528634369373, "Power: dominance attained": 0.0006786694866605103, "Power: dominance constrained": 0.0010993105825036764, "Power: resources attained": 0.002530301921069622, "Power: resources constrained": 0.0012299943482503295, "Face attained": 0.0010289846686646342, "Face constrained": 0.000711916305590421, "Security: personal attained": 0.0028435694985091686, "Security: personal constrained": 0.0011656589340418577, "Security: societal attained": 0.0017767588142305613, "Security: societal constrained": 0.0012105898931622505, "Tradition attained": 0.0007454279693774879, "Tradition constrained": 0.00047847192035987973, "Conformity: rules attained": 0.00042179939919151366, "Conformity: rules constrained": 0.000527711003087461, "Conformity: interpersonal attained": 0.0006553333369083703, "Conformity: interpersonal constrained": 0.0019901844207197428, "Humility attained": 0.0025867691729217768, "Humility constrained": 0.5001301169395447, "Benevolence: caring attained": 0.0007135719060897827, "Benevolence: caring constrained": 0.00018532299145590514, "Benevolence: dependability attained": 0.0022150827571749687, "Benevolence: dependability constrained": 0.00013899922487325966, "Universalism: concern attained": 0.003934443928301334, "Universalism: concern constrained": 0.00030335880001075566, "Universalism: nature attained": 0.02208559960126877, "Universalism: nature constrained": 0.0023718939628452063, "Universalism: tolerance attained": 0.0012924829497933388, "Universalism: tolerance constrained": 0.0005632561515085399}]}
+
+
 
 ################################################################################
 ################################################################################
@@ -115,11 +123,122 @@ def thresholds_vectorized(data):
             lambda x: map_to_confidence(x, thresh)
         )(data[:, i]))
 
-    return check.tolist()[0]
+    return check.tolist()
 
 
 ################################################################################
 ################################################################################
+
+def predict_context_based(input_list: List) -> List:
+    prediction_list = []
+
+    prev_sent_1 = None
+    prev_sent_2 = None
+
+    for entry in tqdm(input_list):
+        if entry.get('Sentence-ID', 1) == 1:
+            prev_sent_1 = prev_sent_2 = None
+
+        text = entry['Text']
+        full_text = text
+        if prev_sent_1 is None and prev_sent_2 is not None:
+            full_text = prev_sent_2 + text
+        elif prev_sent_1 is not None and prev_sent_2 is not None:
+            full_text = prev_sent_1 + prev_sent_2 + text
+
+        temp = {"Text": full_text, 'language': entry.get('language', lang_dict['EN'])}
+        temp.update(tokenize(full_text))
+
+        temp = datasets.Dataset.from_dict({key: [value] for key, value in temp.items()})
+
+        predictions, label, metrics = trainer.predict(temp, metric_key_prefix="predict")
+        # predictions is numpy.ndarray
+
+        pred_values = predictions[0]
+        confidence_values = thresholds(pred_values)
+
+        prediction_list.append(confidence_values)
+
+        prev_sent_1 = prev_sent_2
+
+        text_labels = [f"<{id2label[j]}>" for j, value in enumerate(confidence_values) if value >= 0.5]
+
+        if len(text_labels) > 0:
+            prev_sent_2 = f"{text} <{' '.join(text_labels)}> </s> "
+        else:
+            prev_sent_2 = f"{text} <NONE> </s> "
+
+    return prediction_list
+
+
+def predict_zero_shot(input_list: List) -> List:
+    data = datasets.Dataset.from_pandas(pd.DataFrame.from_records(input_list))\
+        .map(preprocess_function, batched=True, load_from_cache_file=False)
+    predictions, label, metrics = trainer.predict(data, metric_key_prefix="predict")
+    confidence_values = thresholds_vectorized(predictions)
+
+    return confidence_values
+
+
+def predict(input_list: List, validate_input: bool = True) -> List:
+    if len(input_list) == 0:
+        return []
+    if validate_input:
+        data_list = []
+        old_language = 0
+        for i, entry in enumerate(input_list):
+            if isinstance(entry, str):
+                # default to english text
+                current_language = lang_dict['EN']
+                data_list.append({
+                    'Sentence-ID': 1 if current_language != old_language else i+1,
+                    'Text': entry,
+                    'language': lang_dict['EN']
+                })
+
+            elif isinstance(entry, dict) and 'Text' in entry.keys() and isinstance(entry['Text'], str):
+
+                if 'language' in entry.keys():
+                    if isinstance(entry['language'], int) and 0 <= entry['language'] < len(lang_dict):
+                        current_language = lang_dict[entry['language']]
+                    elif isinstance(entry['language'], str):
+                        current_language = lang_dict.get(entry['language'], None)
+                    else:
+                        current_language = None
+                else:
+                    current_language = lang_dict['EN']
+
+                if current_language is None:
+                    logging.error(f"Unrecognized language: {entry['language']}")
+                    continue
+
+                if current_language != old_language:
+                    sentence_id = 1
+                elif 'Sentence-ID' in entry.keys():
+                    if isinstance(entry['Sentence-ID'], int) and 0 <= entry['Sentence-ID']:
+                        sentence_id = entry['Sentence-ID']
+                    else:
+                        logging.error(f"Unrecognized Sentence-ID: {entry['Sentence-ID']}")
+                        sentence_id = i+1
+                else:
+                    sentence_id = i+1
+
+                data_list.append({
+                    'Sentence-ID': sentence_id,
+                    'Text': entry['Text'],
+                    'language': current_language
+                })
+            else:
+                logging.error(f'Unable to format input: {entry}')
+    else:
+        data_list = input_list
+
+    if ZERO_SHOT:
+        prediction_list = predict_zero_shot(data_list)
+    else:
+        prediction_list = predict_context_based(data_list)
+    result = pd.DataFrame(prediction_list, columns=labels)
+    return result.to_dict('records')
 
 
 if __name__ == "__main__" or is_running_as_inference_server():
@@ -127,7 +246,8 @@ if __name__ == "__main__" or is_running_as_inference_server():
     finetuned_tokenizer = '/tokenizer/SotirisLegkas/multi-head-xlm-xl-tokens-38'
     tokenizer = transformers.AutoTokenizer.from_pretrained(finetuned_tokenizer)
     model = MultiHead_MultiLabel_XL.from_pretrained(finetuned_model, problem_type="multi_label_classification")
-    trainer = transformers.Trainer(model=model, args=TrainingArguments(finetuned_model, disable_tqdm=True))
+    trainer = transformers.Trainer(model=model, args=TrainingArguments(finetuned_model, disable_tqdm=not ZERO_SHOT))
+    logging.info(f'Running {"zero-shot" if ZERO_SHOT else "context-based"} prediction')
     
     if not is_running_as_inference_server():
 
@@ -136,49 +256,10 @@ if __name__ == "__main__" or is_running_as_inference_server():
 
         prediction_dataset = load_dataset(input_directory)
 
-        prediction_list = []
-
-        prev_sent_1 = None
-        prev_sent_2 = None
-
-        for i in tqdm(range(len(prediction_dataset))):
-            if prediction_dataset.at[i, 'Sentence-ID'] == 1:
-                prev_sent_1 = prev_sent_2 = None
-
-            text = prediction_dataset.at[i, "Text"]
-            full_text = text
-            if prev_sent_1 is None and prev_sent_2 is not None:
-                full_text = prev_sent_2 + text
-            elif prev_sent_1 is not None and prev_sent_2 is not None:
-                full_text = prev_sent_1 + prev_sent_2 + text
-            
-            temp = {"Text": full_text, 'language': int(prediction_dataset["language"][i])}
-            temp.update(tokenize(full_text))
-            
-            temp = datasets.Dataset.from_dict({key: [value] for key, value in temp.items()})
-            
-            predictions, label, metrics = trainer.predict(temp, metric_key_prefix="predict")
-            # predictions is numpy.ndarray
-            
-            pred_values = predictions[0]
-            confidence_values = thresholds(pred_values)
-
-            prediction_list.append(confidence_values)
-            
-            prev_sent_1 = prev_sent_2
-            
-            text_labels = [f"<{id2label[j]}>" for j, value in enumerate(confidence_values) if value >= 0.5]
-
-            if len(text_labels) > 0:
-                prev_sent_2 = f"{text} <{' '.join(text_labels)}> </s> "
-            else:
-                prev_sent_2 = f"{text} <NONE> </s> "
-            
-        result = pd.DataFrame(prediction_list, columns=labels)
+        result = predict(prediction_dataset.to_dict('records'), validate_input=False)
 
         df_prediction = pandas.DataFrame(
             {'Text-ID': prediction_dataset['Text-ID'], 'Sentence-ID': prediction_dataset['Sentence-ID']})
-        df_prediction = pd.concat([df_prediction, result], axis=1)
+        df_prediction = pd.concat([df_prediction, pd.DataFrame.from_records(result)], axis=1)
 
         write_tsv_dataframe(output_file, df_prediction)
-
